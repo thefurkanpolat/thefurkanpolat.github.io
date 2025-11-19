f2d947f20fdb54efa2d42bfbeb920a1823fd2142
Furkan Polat <fpolat377@gmail.com>
2025-11-19 18:11:35 +0300

    Create scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // 1. TYPED.JS (Yazı Efekti)
+    var typed = new Typed(".element", {
+      strings: ["Information Technology Specialist", "Software Developer", "UI/UX Designer"],
+      typeSpeed: 50, backSpeed: 30, startDelay: 500, loop: true
+    });
+
+    // 2. PARTICLES.JS (Yıldız Efekti)
+    particlesJS("particles-js", {
+        "particles": {
+            "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+            "color": { "value": "#ffffff" },
+            "shape": { "type": "circle" },
+            "opacity": { "value": 0.5, "random": false },
+            "size": { "value": 3, "random": true },
+            "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 },
+            "move": { "enable": true, "speed": 4, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }
+        },
+        "retina_detect": true
+    });
+});
